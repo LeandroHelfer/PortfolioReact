fafc942d3a59b884818bad1a7af1cbbb2450c4b6
LeandroHelfer <leandrohelfer@gmail.com>
2023-02-26 10:45:39 -0300

    finalizando a parte central da pagina

--- src/componentes/Central/Central.js
+++ src/componentes/Central/Central.js
@@ -1,24 +1,41 @@
 import './Central.css'
+import fotoperfil from '../Assets/img.svg'
+import card1 from '../Assets/card1.svg'
+import card2 from '../Assets/card2.svg'
+import card3 from '../Assets/card3.svg'
+import card4 from '../Assets/card4.svg'
 
 const Central = () => {
     return (
         <div class="container-fluid text-center bg-primary">
             <div class="row">
-                <div class="col-sm-8 border">
-                    <h1>Olá, sou o Leandro</h1>
-                    <h3>Sou desenvolvedor, e gostaria de apresentar meu <br></br>
-                    trabalho para você através deste portfólio.</h3>
+                <div class="col-sm-8 mt-5 pt-4">
+                    <h1 class="text-white text-end font-family-poppins fw-bold pe-5 me-5 font-sizeh1">Olá, sou o Leandro</h1>
+                    <h3 class="text-white text-end font-family-poppins fs-4 fw-light text-justify">Sou desenvolvedor, e gostaria de apresentar meu <br></br>
+                        trabalho para você através deste portfólio.</h3>
                 </div>
-                <div class="col-sm-4 border">col-sm-4</div>
+                <div class="col-sm-4">
+                    <img class="pt-5 pb-5 mb-5" src={fotoperfil}></img>
+                </div>
             </div>
             <div class="row">
-                <div class="col-sm">col-sm</div>
+                <div class="col-sm">
+                    <h1 class="text-white font-family fs-2" >Meus Projetos</h1>
+                </div>
             </div>
-            <div class="row">
-                <div class="col-sm">col-sm</div>
-                <div class="col-sm">col-sm</div>
-                <div class="col-sm">col-sm</div>
-                <div class="col-sm">col-sm</div>
+            <div class="row ms-3 me-3 pb-5">
+                <div class="col-sm">
+                    <img class="pt-4" src={card1}></img>
+                </div>
+                <div class="col-sm">
+                    <img class="pt-4" src={card2}></img>
+                </div>
+                <div class="col-sm">
+                    <img class="pt-4" src={card3}></img>
+                </div>
+                <div class="col-sm">
+                    <img class="pt-4" src={card4}></img>
+                </div>
             </div>
         </div>
     )
--- src/componentes/navbar/Navbar.js
+++ src/componentes/navbar/Navbar.js
@@ -2,22 +2,22 @@ import './Navbar.css'
 
 const Navbar = () => {
     return (
-        <nav class="navbar navbar-expand-lg bg-white">
+        <nav class="navbar navbar-expand-lg bg-white shadow p-0">
             <div class="container-fluid">
                 <div class="collapse navbar-collapse" id="navbarText">
                     <ul class="navbar-nav me-auto mb-2 mb-lg-0 px-5">
-                        <a class="navbar-brand font-family text-shadow" href="#">Home</a>
+                        <a class="navbar-brand font-family text-shadow fw-bolder" href="#">Home</a>
                     </ul>
                     <span class="navbar-text">
                         <ul class="navbar-nav me-auto mb-2 mb-lg-0 px-5">
                             <li class="nav-item px-5">
-                                <a class="nav-link font-family" aria-current="page" href="#">Sobre Mim</a>
+                                <a class="nav-link font-family fw-bolder" aria-current="page" href="#">Sobre Mim</a>
                             </li>
                             <li class="nav-item px-5">
-                                <a class="nav-link font-family" href="#">Meus Projetos</a>
+                                <a class="nav-link font-family fw-bolder" href="#">Meus Projetos</a>
                             </li>
                             <li class="nav-item px-5">
-                                <a class="nav-link font-family" href="#">Fale Comigo</a>
+                                <a class="nav-link font-family fw-bolder" href="#">Fale Comigo</a>
                             </li>
                         </ul>
                     </span>
@@ -25,6 +25,7 @@ const Navbar = () => {
             </div>
         </nav>
     )
+    
 }
 
 export default Navbar
